8938f30d2e7f009fadf65f296be52e75ed7f14ca
SrijehsKhanal <khanal.srijesh@gmail.com>
2023-11-24 09:18:11 -0500

    Updated output

--- app.py
+++ app.py
@@ -9,16 +9,15 @@ NEWS_API_KEY = "fc2a6edc94fc44fdbfb7e1319643bfdf"
 def homepage():
     return render_template("index.html")
 
-@app.route("/newsletterinfo", methods=["POST"])
 def get_newsletter_info():
     newsletter_name = request.form.get("newsletter")
     api_key = request.form.get('apikey')
 
-    # Verify that the API key matches your expected API key
+    # Verifing that the API key matches expected API key
     if api_key != NEWS_API_KEY:
         return render_template("error.html", error_message="Invalid API Key")
 
-    # Make a request to the News API
+    # Request to the News API
     url = f"https://newsapi.org/v2/everything?q={newsletter_name}&apiKey={NEWS_API_KEY}"
 
     try:
@@ -26,19 +25,21 @@ def get_newsletter_info():
         response.raise_for_status()
         data = response.json()
 
-        print("API Response:", data)  # For debugging
-
         if response.status_code == 200 and data.get('status') == 'ok':
-            # Choose the first article for simplicity, you can modify this based on your needs
             articles = data.get('articles', [])
+            # Taking only first data
             if articles:
                 first_article = articles[0]
                 return render_template("newsletterinfo.html", newsletter_data=[first_article])
             else:
                 return render_template("error.html", error_message="No articles found.")
         else:
-            error_message = data.get('message', 'ERROR!!')
-            return render_template("error.html", error_message=error_message)
+            #  Error message 
+            if data.get('message'):
+                error_message = data.get('message')
+                return render_template("error.html", error_message=error_message)
+            else:
+                return render_template("error.html", error_message="An unexpected error occurred.")
 
     except requests.exceptions.HTTPError as errh:
         print("HTTP Error:", errh)  # For debugging
